e8408d5e8c88c5235453a8022e489666a70991eb
johngorithm <johngorithm@gmail.com>
2018-07-01 08:20:22 +0100

    FEATURE : CREATE UI INTERFACE TO CONSUME CURRENCY CONVERTER API
    This consume currency converter api with a neatly designed user interface,
    take care of errors and invalid data input
    
    FINISHED : APP WORKS

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+const result = document.getElementById('result');
+const messageOut = document.getElementById('message');
+
+function convertCurrency() {
+  let amount = document.getElementById('amount').value;
+  let fromCurrency = document.getElementById('from').value;
+  let toCurrency = document.getElementById('to').value;
+  if (amount.trim() === ''){
+    alert('No data entered, please try again');
+    return;
+  }
+  if(isNaN(amount)) {
+    alert('Invalid data entered, please try again');
+    return;
+  } 
+
+
+  fromCurrency = encodeURIComponent(fromCurrency);
+  toCurrency = encodeURIComponent(toCurrency);
+  let query = fromCurrency + '_' + toCurrency;
+
+  let url = 'https://free.currencyconverterapi.com/api/v5/convert?q='+ query + '&compact=ultra';
+
+	fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsonData) => {
+		if(jsonData) {
+			let total = jsonData[query] * Number(amount);
+      total = Math.round(total * 100) / 100
+			result.value = total;
+		}
+    
+  }).catch(error => {
+    alert('Error: Data was not fetched')
+  });
+}
+
+
+//uncomment to test
+const convertBtn = document.getElementById('convert-btn');
+convertBtn.onclick = (event) => {
+	convertCurrency();
+}
+
+
+
